f52f621e368c86283e075fd9b37eaf8bc736f8af
Rodrigo Guerrero Zerbini <rodrigo.zerbini@gmail.com>
2022-06-16 20:33:18 +0200

    Update frogJumps.java

--- frogJumps/frogJumps.java
+++ frogJumps/frogJumps.java
@@ -1,1 +1,7 @@
-
+import java.lang.Math;
+class Solution {
+    public int solution(int X, int Y, int D) {
+        double jumps = Math.ceil((double) (Y - X) / D);
+        return (int) jumps;
+    }
+}
