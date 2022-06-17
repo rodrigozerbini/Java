980cfa663996e60f625fd4c2becc14f85d581421
Rodrigo Guerrero Zerbini <rodrigo.zerbini@gmail.com>
2022-06-17 20:15:52 +0200

    Update triangle.java

--- triangle/triangle.java
+++ triangle/triangle.java
@@ -14,7 +14,7 @@ class Solution {
     public int solution(int[] A) {
         Arrays.sort(A);
         for (int i = 0; i < A.length - 2; i++) 
-            if (A[i] >= 0 && A[i] > A[i+2] - A[i+1]) 
+            if (A[i] > A[i+2] - A[i+1]) 
                 return 1;
         return 0;
     }
