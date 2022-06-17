23e50edcf915bbcb3bf7498a0a594352d3093b18
Rodrigo Guerrero Zerbini <rodrigo.zerbini@gmail.com>
2022-06-17 19:55:30 +0200

    Create triangle.java

---
+++ triangle/triangle.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class Solution {
+    /*
+    * If the array is sorted,
+    * A[i+2] + A[i+1] > A[i] is always true and
+    * A[i+2] + A[i] > A[i+1] is always true.
+    * So, I just need to check if
+    * A[i+1] + A[i] > A[i+2].
+    * Since in case of A[i] + A[i + 1] > MAXINT the code 
+    * would strike an overflow (the result would be greater
+    * than allowed integer limit), we´ll modify the formula
+    * to an equivalent A[i] > A[i+2] - A[i+1]
+    */
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        for (int i = 0; i < A.length - 2; i++) 
+            if (A[i] >= 0 && A[i] > A[i+2] - A[i+1]) 
+                return 1;
+        return 0;
+    }
+}
