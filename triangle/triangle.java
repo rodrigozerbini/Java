7488898b42a575af0e1816c758f07ee027f391f1
Rodrigo Guerrero Zerbini <rodrigo.zerbini@gmail.com>
2022-06-17 20:13:08 +0200

    Update triangle.java

--- triangle/triangle.java
+++ triangle/triangle.java
@@ -6,8 +6,8 @@ class Solution {
     * A[i+2] + A[i] > A[i+1] is always true.
     * So, I just need to check if
     * A[i+1] + A[i] > A[i+2].
-    * Since in case of A[i] + A[i + 1] > MAXINT the code 
-    * would strike an overflow (the result would be greater
+    * In order to avoid A[i] + A[i + 1] > MAXINT and the code 
+    * striking an overflow (with a result greater
     * than allowed integer limit), we´ll modify the formula
     * to an equivalent A[i] > A[i+2] - A[i+1]
     */
